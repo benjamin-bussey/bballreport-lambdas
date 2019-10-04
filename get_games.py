12f537c7167c643a24bb3430d58cbebf408ba0fb
BuildTools <unconfigured@null.spigotmc.org>
2019-10-04 19:31:52 -0400

    Added comments to data access functions

--- get_games.py
+++ get_games.py
@@ -3,6 +3,10 @@ from boto3.dynamodb.conditions import Key
 
 
 def setup_dynamodb():
+    """
+    Method for creating the dynamodb resource for querying/scanning
+    :return:
+    """
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('bballreport')
 
@@ -10,6 +14,12 @@ def setup_dynamodb():
 
 
 def get_game(table, game_id):
+    """
+    Gets a single game with supplied gameid
+    :param table: supplied table resource to query/scan
+    :param game_id:
+    :return:
+    """
     response = table.query(
         KeyConditionExpression=Key('hashKey').eq('game{}'.format(game_id)) & Key('sortKey').eq('1')
     )
@@ -17,20 +27,32 @@ def get_game(table, game_id):
 
 
 def get_season_games(table, season):
+    """
+    Gets all games for a particular season
+    :param table: supplied table resource to query/scan
+    :param season: the season to get games for
+    :return:
+    """
     response = table.query(
-        IndexName='DailyGames',
-        KeyConditionExpression=Key('hashKey').eq(season)
+        IndexName='SeasonGames',
+        KeyConditionExpression=Key('season').eq(season)
     )
     return response['Items']
 
 
 def get_daily_games(table, season, date):
+    """
+    Gets all games for a particular day
+    :param table: supplied table resource to query/scan
+    :param season: the season to get games for
+    :param date: the date to get games for
+    :return:
+    """
     response = table.query(
-        IndexName='DailyGames',
-        KeyConditionExpression=Key('hashKey').eq(season) & Key('sortKey').eq(date)
+        IndexName='SeasonGames',
+        KeyConditionExpression=Key('season').eq(season) & Key('date').eq(date)
     )
     return response['Items']
-
 
 
 def get_season_games_handler(event, context):
@@ -38,7 +60,6 @@ def get_season_games_handler(event, context):
     return get_season_games(table, event['season'])
 
 
-# ToDo implement after adding GSI on season, date
 def get_daily_games_handler(event, context):
     table = setup_dynamodb()
     return get_daily_games(table, event['season'], event['date'])
--- get_players.py
+++ get_players.py
@@ -3,6 +3,10 @@ from boto3.dynamodb.conditions import Key
 
 
 def setup_dynamodb():
+    """
+    Method for creating the dynamodb resource for querying/scanning
+    :return:
+    """
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('bballreport')
 
@@ -10,23 +14,42 @@ def setup_dynamodb():
 
 
 def get_player(table, player_id):
+    """
+    Method for retrieving all entries for a player across multiple seasons and teams
+    :param table: supplied table resource to query/scan
+    :param player_id: the id of the player
+    :return:
+    """
     response = table.query(
         KeyConditionExpression=Key('hashKey').eq('player{}'.format(player_id))
     )
     return response['Items']
 
 
-def get_player_team(table, player_id, team_id):
+def get_player_team(table, player_id, season, team_id):
+    """
+    Gets information about a player for a specific season for a specific team
+    :param table: supplied table resource to query/scan
+    :param player_id: the id of the player
+    :param team_id: the id of the team
+    :return:
+    """
     response = table.query(
-        KeyConditionExpression=Key('hashKey').eq('player{}'.format(player_id)) & Key('sortKey').eq('2019-2020|{}'.format(team_id))
+        KeyConditionExpression=Key('hashKey').eq('player{}'.format(player_id)) & Key('sortKey').eq('{}|{}'.format(season, team_id))
     )
     return response['Items']
 
 
 def get_team_players(table, team_id):
+    """
+
+    :param table: supplied table resource to query/scan
+    :param team_id: the id of the team
+    :return:
+    """
     response = table.query(
         IndexName='TeamPlayers',
-        KeyConditionExpression=Key('hashKey').eq(team_id)
+        KeyConditionExpression=Key('currentTeam').eq(team_id)
     )
     return response['Items']
 
@@ -36,11 +59,11 @@ def get_player_handler(event, context):
     return get_player(table, event['playerid'])
 
 
-def get_player_team_handler(event, context)
+def get_player_team_handler(event, context):
     table = setup_dynamodb()
-    return get_player_team(table, event['playerid'], event['teamid'])
+    return get_player_team(table, event['playerid'], event['season'], event['teamid'])
+
 
 def get_team_players_handler(event, context):
     table = setup_dynamodb()
     return get_team_players(table, event['teamid'])
-
